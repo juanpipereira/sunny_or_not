8a27617b72b18442325cc49c6bd9d10ccce5013e
Juan Pablo Pereira <juanpifreelancer@gmail.com>
2026-02-27 14:44:41 -0300

    Feat: Incorporate dark mode support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sunny_or_not/core/theme/gradient_theme.dart';
+import 'package:sunny_or_not/core/theme/app_theme.dart';
 import 'package:sunny_or_not/di_setup.dart';
 import 'package:sunny_or_not/features/gps/presentation/bloc/gps_bloc.dart';
 import 'package:sunny_or_not/features/location/presentation/bloc/location_bloc.dart';
@@ -18,38 +18,13 @@ void main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  static final Color blue300 = Colors.blue.shade300;
-  static final Color blue600 = Colors.blue.shade600;
-  static final Color background = Colors.grey.shade50;
-  static const Color mainWhite = Colors.white;
-  static const Color secondaryWhite = Colors.white70;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sunny or not',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: blue600,
-          primary: blue600,
-          secondary: blue300,
-          surface: background,
-          onPrimaryContainer: mainWhite,
-          onSecondaryContainer: secondaryWhite,
-        ),
-        scaffoldBackgroundColor: background,
-        extensions: [
-          GradientTheme(
-            mainGradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [blue300, blue600],
-            ),
-          ),
-        ],
-      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       home: MultiBlocProvider(
         providers: [
           BlocProvider<GpsBloc>(
